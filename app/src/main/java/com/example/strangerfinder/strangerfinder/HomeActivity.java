141ba470df445dd06cc555872847c3aeef9f5762
Jose Alvarez <josealvarezvivar@gmail.com>
2017-11-16 21:39:04 +0100

    an attemp of register users, the still in the firebase permanently

--- app/src/main/java/com/example/strangerfinder/strangerfinder/HomeActivity.java
+++ app/src/main/java/com/example/strangerfinder/strangerfinder/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.example.strangerfinder.strangerfinder;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import com.example.strangerfinder.strangerfinder.Models.Usuario;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -19,6 +27,8 @@ public class HomeActivity extends AppCompatActivity {
     RadioButton rbSexChose;
     RadioButton rbPreferenceChose;
     LinearLayout lyMenu;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +49,54 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        btStartToChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isEvererythingCorrect;
+
+                //STEP 1: verifying the username is correct
+                isEvererythingCorrect = !etUsername.getText().toString().isEmpty();
+
+                //STEP 2: verifying that the kind of sex has been chosen
+                if(isEvererythingCorrect)
+                    isEvererythingCorrect = verifyRadioButtons(rgSex);
+                else{
+                    etUsername.setError("You should choose an username");
+                }
+                //STEP 3: verifying that the preference has been chosen
+                if(isEvererythingCorrect)
+                    isEvererythingCorrect = verifyRadioButtons(rgPreference);
+
+                //STEP 4: Creating a user and searching a partner
+                if(isEvererythingCorrect){
+
+                    //STEP 1: connecting with firebase
+                    database = FirebaseDatabase.getInstance();
+                    myRef = database.getReference("free_users");
+
+                    //STEP 2: creating the user
+                    Usuario user = new Usuario();
+                    user.setNombre(etUsername.getText().toString());
+                    user.setPreferencia(1);
+                    user.setSexo(0);
+
+                    Log.e("USER",user.toString());
+                    //STEP 3: going to free_user and generate a new user with data
+                    myRef.push().setValue(user);
+                }
+
+            }
+        });
+
+    }
+
+    private boolean verifyRadioButtons(RadioGroup rg){
+        boolean isChosen = false;
+
+        if(rg.getCheckedRadioButtonId() != 0){
+            isChosen = true;
+        }
+
+        return isChosen;
     }
 }
