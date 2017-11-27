08e3a76204ab921150334448d100136fd95a14c4
Jose <josealvarezvivar@gmail.com>
2017-11-27 20:11:56 +0100

    some design added

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -19,6 +19,6 @@ public final class R {
         public static final int google_play_services_version = 0x7f080003;
     }
     public static final class string {
-        public static final int common_google_play_services_unknown_issue = 0x7f0b0020;
+        public static final int common_google_play_services_unknown_issue = 0x7f0b0022;
     }
 }
